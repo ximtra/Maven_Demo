78e6e8f5e7e2e134985be298132057361edf929f
Karim Afifi (kafifi) <karim.afifi@ximtra.com>
2016-09-03 21:47:05 -0400

    Maven V1

---
+++ Maven_Demo/src/main/java/Maven_Demo/Maven_Demo/MyDemp.java
@@ -0,0 +1,11 @@
+package Maven_Demo.Maven_Demo;
+
+public class MyDemp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a Maven Demo");
+
+	}
+
+}
